a4d42e017d754828370c393c56d4d5f53746d79f
sherrichuah <sherrichuah4@gmail.com>
2024-10-12 22:14:03 +0100

    review page css change

--- client/src/components/FormSteps/ReviewStep/ReviewStep.jsx
+++ client/src/components/FormSteps/ReviewStep/ReviewStep.jsx
@@ -1,4 +1,4 @@
-import { LeftDiv } from "./ReviewStepStyle";
+import { Div, Heading, BigDiv } from "./ReviewStepStyle";
 
 export const ReviewStep = ({inputValue}) => {
     const [destinationValue, travelDates, peopleDetails] = inputValue;
@@ -9,46 +9,49 @@ export const ReviewStep = ({inputValue}) => {
 
     return (
         <div>
-            <LeftDiv>
-                <h2>Destination Airport</h2>
-                <p>{destinationValue.airport_name} ({destinationValue.iata})</p>
-                <p>{destinationValue.city}, {destinationValue.country}</p>
-            </LeftDiv>
+            <BigDiv>
+                <Div>
+                    <Heading>Destination Airport</Heading>
+                    <p>{destinationValue.airport_name} ({destinationValue.iata})</p>
+                    <p>{destinationValue.city}, {destinationValue.country}</p>
+                </Div>
+                <Div>
+                    <Heading>Travel Dates</Heading>
+                    {travelDates.map((date, index) => (
+                        <div>
+                            <p>From: {date.startDate.toLocaleDateString('en-GB', {
+                                day: 'numeric',
+                                month: 'long',
+                                year: 'numeric',
+                                })};
+                            </p>
+                            <p>To: {date.endDate.toLocaleDateString('en-GB', {
+                                day: 'numeric',
+                                month: 'long',
+                                year: 'numeric',
+                                })};
+                            </p>
+                        </div>
+                    ))}
+                </Div>
+            </BigDiv>
             <br/>
-            <LeftDiv>
-                <h2>Travel Dates</h2>
-                {travelDates.map((date, index) => (
-                    <div>
-                        <p>From: {date.startDate.toLocaleDateString('en-GB', {
-                            day: 'numeric',
-                            month: 'long',
-                            year: 'numeric',
-                            })};
-                        </p>
-                        <p>To: {date.endDate.toLocaleDateString('en-GB', {
-                            day: 'numeric',
-                            month: 'long',
-                            year: 'numeric',
-                            })};
-                        </p>
-                    </div>
-                ))}
-            </LeftDiv>
-            <br/>
-            <LeftDiv>
-                <h2>People Details</h2>
-                {peopleDetails.map((peopleDetail, index) => (
-                    <LeftDiv key={peopleDetail.id}>
+            <BigDiv>
+                <div>
+                    <Heading>People Details</Heading>
+                </div>
+                <br />             {peopleDetails.map((peopleDetail, index) => (
+                    <Div key={peopleDetail.id}>
                         <p>Group Name: {peopleDetail.group}</p>
                         <p>Origin Airport: {peopleDetail.originAirport.airport_name}, ({peopleDetail.originAirport.iata})</p>
                         <p>Depart After: {peopleDetail.departAfter}</p>
                         <p>Arrive Before: {peopleDetail.arriveBefore}</p>
                         <p>Cabin Class: {peopleDetail.cabinClass}</p>
                         <p>Direct Flight: {peopleDetail.directFlight}</p>
-                    </LeftDiv>
+                    </Div>
                 ))}
                 <br/>
-            </LeftDiv>
+            </BigDiv>
 
         </div>
     )
--- client/src/components/FormSteps/ReviewStep/ReviewStepStyle.jsx
+++ client/src/components/FormSteps/ReviewStep/ReviewStepStyle.jsx
@@ -1,11 +1,33 @@
 import styled from 'styled-components'
 
-export const LeftDiv = styled.div`
-    // float: left;      
-    // width: 200px;     
+export const BigDiv = styled.div`
+    // // float: left;      
+    // // width: 200px;     
     margin: 1px;   
     text-align: justify;
     border: 1px solid black;
-    padding-left: 20px; 
-    padding-right: 20px; 
+    padding: 10px;
+    padding:1rem;
+    display:block;
+
+    @media (min-width: 400px) {
+    }
+
+    @media (min-width: 600px) {
+        display: grid;
+        grid-template-columns: 1fr 1fr;
+    }
+`
+
+export const Div = styled.div`
+    float: left;   
+    margin: 1px;   
+    text-align: center;
+    border: 1px solid black;
+    padding: 10px; 
+    
+`
+
+export const Heading = styled.h2`
+    font-size:1.2rem;
 `
